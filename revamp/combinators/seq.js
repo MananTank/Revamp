7e7515381c1698ba81c4aa8350f9eaa496ef75ca
Manan Tank <manan.tank@tcs.com>
2020-07-05 13:47:42 +0530

    seq is now object

--- revamp/combinators/seq.js
+++ revamp/combinators/seq.js
@@ -1,37 +1,36 @@
+/* eslint-disable guard-for-in */
 const createParser = require('../utils/createParser');
 
 // a strict seq throws strictError when it is not fully satisfied
 // if not even the first parser is satisfied, then no criticle error is thrown, only simple error
 
-function seq(parsers, op = {}) {
-  // for debug mode
-  const parserList = parsers.map((p) => p.parses).join(',');
+function seq(parsersObj, op = {}) {
+  const totalParsers = Object.keys(parsersObj).length;
 
-  // keep parsing parsers till an error is encountered
-  // return last state, with parsed value changed to array of parsed values
   function logic(state) {
     let newState = state;
-    const parsedArray = [];
+    const parsedObj = {};
 
-    for (const parser of parsers) {
+    for (const key in parsersObj) {
+      const parser = parsersObj[key];
       newState = parser(newState);
       // ❌ stop parsing further
       // do not send half parsed seq
       if (newState.error) return { ...newState, parsed: null };
-      parsedArray.push(newState.parsed);
+      parsedObj[key] = newState.parsed;
     }
 
     // ✔️, ❌ return last parser's state, but change the parsed value to parsed array
 
+    const parsedParsers = Object.keys(parsersObj).length;
     // if some parsed but not all
-    if (op.strict && parsedArray.length > 0 && parsedArray.length !== parsers.length) {
-      console.log('seq not fully satisfied: ', parsedArray);
-      return { ...newState, strictError: true, parsed: parsedArray };
+    if (op.strict && parsedParsers > 0 && totalParsers !== parsedParsers) {
+      return { ...newState, strictError: true, parsed: parsersObj };
     }
-    return { ...newState, parsed: parsedArray };
+    return { ...newState, parsed: parsedObj };
   }
 
-  return createParser(logic, op, { type: 'seq', parses: `${parserList}` });
+  return createParser(logic, op, { type: 'seq', parses: parsersObj });
 }
 
 module.exports = seq;
